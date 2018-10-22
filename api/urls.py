05deefb50cb4150c49d5f5180d575fb1628b15c1
Oleg Marintsov <awwert@yandex.ru>
2018-10-23 00:04:33 +0700

    Add API app.

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from django.conf import settings
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='TODO API')
+
+urlpatterns = [
+    url(r'^', include('loginsys.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^auth/', include('rest_framework.urls')),
+        url(r'^docs/$', schema_view)
+    ]
